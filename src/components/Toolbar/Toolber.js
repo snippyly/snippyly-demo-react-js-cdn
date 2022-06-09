46f98ed392241d294ccfa83c681ccfc5a9d9281a
vivekk-snippyly <vivek@snippyly.com>
2022-06-10 02:56:54 +0530

    Updated snippyly to 1.0.39. Added stream view screen.

--- src/components/Toolbar/Toolber.js
+++ src/components/Toolbar/Toolber.js
@@ -43,9 +43,18 @@ function Toolbar() {
         setSelectedUser(user);
     }
 
+    const navigateTo = (path, target = '_self') => {
+        window.open(path, target);
+    }
+
     return (
         <div className='header'>
             <snippyly-presence></snippyly-presence>
+            <div className='menu-container'>
+                <span className='menu' onClick={() => navigateTo('/')}>Home</span>
+                <span className='menu' onClick={() => navigateTo('/stream-view')}>Stream View</span>
+                <span className='menu' onClick={() => navigateTo('https://snippyly-demo-react-js-cdn-wdp.web.app/', '_blank')}>Document Params</span>
+            </div>
             <div>
                 {
                     selectedUser ?
